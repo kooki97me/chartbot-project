5e523e04d308ed0129b730be553aa20ccd992148
kooki97me <komalthakur150197@gmail.com>
2025-06-08 16:03:34 +0530

    make changes to live on render

--- chatbot/engine.py
+++ chatbot/engine.py
@@ -1,12 +1,8 @@
 import nltk
 import spacy
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except:
-    import os
-    os.system("python -m spacy download en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+# The spaCy model 'en_core_web_sm' should be installed via requirements.txt
+nlp = spacy.load("en_core_web_sm")
 
 def respond(text):
     doc = nlp(text)
